8e9528b1cfa218f07d1a0ee1d8c04b3f9a4e41ad
Tyler Johnson <tajo254@uky.edu>
2022-11-06 13:05:40 -0500

    added questions

--- chatbot.js
+++ chatbot.js
@@ -19,10 +19,23 @@ question2.setValidAnswers(["red", "blue", "purple", "yellow", "pink", "green", "
 question2.setFilterPhrases(["my favorite color is ", " is my favorite color", " is the color", "the color is ",
                             "it is ", "that is ", "that would be ", "probably ", "obviously "]);
 
+let question3 = new Question();
+question3.setQuestion("What should the title of this webpage be?");
+question3.setValidAnswers([]);
+question3.setFilterPhrases(["the title should be", "we should call it", "is the title", "the title is", "it should be",
+    "probably", "obviously", "make it", "set it to", "call it", "you should call it"]);
+
+let question4 = new Question();
+question4.setQuestion("What would you like to call me?");
+question4.setValidAnswers([]);
+question4.setFilterPhrases(["i hereby dub you", "your name is", "you should be called", "i will name you", "is your name", "you are"
+    , "how about", "what about", "it should be", "you should be", "i would like to call you", "from now on you should be"]);
 const firstState = new State();
 firstState.setName("Introduction");
 firstState.setQuestions(question1);
 firstState.setQuestions(question2);
+firstState.setQuestions(question3);
+firstState.setQuestions(question4);
 
 //function to change background color
 function updateBackgroundColor(color) {
